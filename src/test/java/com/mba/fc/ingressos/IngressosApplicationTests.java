f35d9a84b7c7a848bdb2572a31a5388230cba208
antonialucianapires <antonialucianapires@gmail.com>
2026-06-14 11:16:44 -0300

    chore: configurações iniciais

---
+++ src/test/java/com/mba/fc/ingressos/IngressosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mba.fc.ingressos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
