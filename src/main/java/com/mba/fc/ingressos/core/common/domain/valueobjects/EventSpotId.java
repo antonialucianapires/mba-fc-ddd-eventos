6107e9e96ea412a5e14faa72ee35d7962bc8365e
antonialucianapires <antonialucianapires@gmail.com>
2026-06-28 14:36:34 -0300

    feat: adiciona entidade EventSpot e value object EventSpotId

---
+++ src/main/java/com/mba/fc/ingressos/core/common/domain/valueobjects/EventSpotId.java
@@ -0,0 +1,11 @@
+package com.mba.fc.ingressos.core.common.domain.valueobjects;
+
+public class EventSpotId extends Uuid {
+    public EventSpotId() {
+        super();
+    }
+
+    public EventSpotId(String value) {
+        super(value);
+    }
+}
---
+++ src/main/java/com/mba/fc/ingressos/core/events/domain/entities/EventSpot.java
@@ -0,0 +1,57 @@
+package com.mba.fc.ingressos.core.events.domain.entities;
+
+import com.mba.fc.ingressos.core.common.domain.Entity;
+import com.mba.fc.ingressos.core.common.domain.valueobjects.EventSpotId;
+
+public class EventSpot extends Entity<EventSpotId> {
+
+    private final String location;
+    private final boolean isReserved;
+    private final boolean isPublished;
+
+    public EventSpot(String location, boolean isReserved, boolean isPublished) {
+        super(new EventSpotId());
+        this.location = location;
+        this.isReserved = isReserved;
+        this.isPublished = isPublished;
+    }
+
+    public EventSpot(String id, String location, boolean isReserved, boolean isPublished) {
+        super(new EventSpotId(id));
+        this.location = location;
+        this.isReserved = isReserved;
+        this.isPublished = isPublished;
+    }
+
+    public EventSpot(EventSpotId id, String location, boolean isReserved, boolean isPublished) {
+        super(id);
+        this.location = location;
+        this.isReserved = isReserved;
+        this.isPublished = isPublished;
+    }
+
+    public static EventSpot create(String location) {
+        return new EventSpot(new EventSpotId(), location, false, false);
+    }
+
+    public EventSpotId getId() {
+        return id;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public boolean isReserved() {
+        return isReserved;
+    }
+
+    public boolean isPublished() {
+        return isPublished;
+    }
+
+    @Override
+    public String toString() {
+        return "EventSpot{id=" + id.getValue() + ", location=" + location + "}";
+    }
+}
